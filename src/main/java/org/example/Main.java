eedf758302369e427e7024011158773e0c2af241
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-06-25 19:16:01 +0200

    created gui class to manage the graphical user interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
-import org.example.controller.Gui;
+import org.example.model.Gui;
+import org.example.model.pages.Landing;
 
+/**
+ * Main class of the program
+ * Starts the program
+ * */
 public class Main {
     public static void main(String[] args) {
         // create and show the window
         Gui gui = new Gui();
+        gui.SetContentPanel(new Landing());
     }
 }
---
+++ src/main/java/org/example/model/pages/Landing.java
@@ -0,0 +1,24 @@
+package org.example.model.pages;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Landing page of the application
+ * */
+public class Landing extends JPanel {
+
+    /**
+     * Constructor that creates the window with all GUI components
+     * */
+    public Landing(){
+        super();
+        this.setBackground(Color.BLUE);
+        /*JLabel lblTitle = new JLabel("Select your mode");
+        lblTitle.setBounds(100,100,150,50);
+        this.add(lblTitle);*/
+    }
+
+
+}
