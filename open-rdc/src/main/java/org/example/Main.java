083ba95adcbc2c0036268883795ebdb2b61c7069
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-06-25 18:14:05 +0200

    create project

---
+++ open-rdc/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.controller.Gui;
+
+public class Main {
+    public static void main(String[] args) {
+        // create and show the window
+        Gui gui = new Gui();
+    }
+}
